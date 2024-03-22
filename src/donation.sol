3f3a1dd0f748b1f1a7e35b6ca45fe59cb84254ca
Stellan Wea <stellan.wea@dauphine.eu>
2024-03-22 15:20:56 +0100

    return address best bidder

--- src/donation.sol
+++ src/donation.sol
@@ -14,9 +14,10 @@ contract Donation is ERC721, Ownable {
     ERC721("GoFundMyLifestyle", "GFML") {}
   
     uint256 donationCounter;
+    mapping(address => uint256) highestBidder;
     address largestDonor;
     uint256 largestOffer;
-    uint256[] offers;
+    uint256[] allOffers;
 
     function mint(address to, uint256 tokenId)  public // uint256 amount
     {
@@ -25,13 +26,13 @@ contract Donation is ERC721, Ownable {
         
     }
 
-    function makeAnOffer (address to, uint256 amount) public {
+    function makeAnOffer (address to, uint256 amount) public returns(address) {
 
         donationCounter++;
 
         bool highest = true;
-        for (uint i=0; i < offers.length; i++) {
-            if (offers[i] < amount) {
+        for (uint i=0; i < allOffers.length; i++) {
+            if (allOffers[i] < amount) {
                  highest = false;
             }
         }
@@ -39,11 +40,14 @@ contract Donation is ERC721, Ownable {
         if (highest == true) {
             largestDonor = to;
             largestOffer = amount;
+            //mapping(address => uint256) highestBidder;
+            highestBidder[to] = amount;
 
         }
 
-        offers.push(amount);
-
+        
+        allOffers.push(amount);
+        return to;
 
     }
 
